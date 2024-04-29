301be934e13ba1f15c4e64e55de67e3c45868ee2
Wumpie7 <chrisjaver@gmail.com>
2024-04-30 01:12:53 +0200

    Added new animals to scene, adjust spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,13 +4,14 @@ namespace Zoo
 {
     internal class Spawner : MonoBehaviour
     {
+        [SerializeField] private Transform parent;
         [SerializeField] private GameObject[] animals;
 
         private void Start()
         {
             foreach (GameObject animal in animals)
             {
-                GameObject newAnimal = Instantiate(animal, transform);
+                GameObject newAnimal = Instantiate(animal, parent);
                 newAnimal.name = animal.name;
             }
         }
